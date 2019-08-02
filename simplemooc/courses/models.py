fa45edc0945449d4c4631aeade2e232a0598fee5
jagra26 <31116911+jagra26@users.noreply.github.com>
2019-08-02 11:31:03 -0300

    seçao 2

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+	def search(self, query):
+		return self.get_queryset().filter(
+			models.Q(name__icontains=query)|\
+			models.Q( description__icontains=query)
+			)
+
 class Course(models.Model):
 
 	name = models.CharField('Nome', max_length=100)
@@ -13,3 +20,12 @@ class Course(models.Model):
 		auto_now_add=True)
 	updated_at = models.DateTimeField ('Atualizado em',
 		auto_now=True)
+	objects = CourseManager()
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Curso'
+		verbose_name_plural = 'Cursos'
+		ordering = ['name']
