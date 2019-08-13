5d15ddc0ecbb90c958caa6e52e3411937b65ba0c
soficastro <38478765+soficastro@users.noreply.github.com>
2019-08-13 09:23:02 -0300

    Update urls.py

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from simplemooc.courses import views
-from simplemooc.courses.views import index
+from simplemooc.courses.views import index, details
 urlpatterns = [ 
-	url(r'^$', index, name='index'),	
+	url(r'^$', index, name='index'),
+	url(r'^(?P<slug>[\w_-]+)/$', details, name='details'),
+	#url(r'^(?P<pk>\d+)/$', details, name='details'),
 ]
