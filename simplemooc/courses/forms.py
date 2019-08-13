1ab9f9026a8e5b65c194fd2d00e39860c1bf9051
soficastro <38478765+soficastro@users.noreply.github.com>
2019-08-13 09:19:54 -0300

    Add files via upload

---
+++ simplemooc/courses/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactCourse(forms.Form):
+	name = forms.CharField(label='Nome', max_length=100)
+	email = forms.EmailField(label='Email')
+	message = forms.CharField(
+		label='Mensagem/Dúvida', widget=forms.Textarea)
