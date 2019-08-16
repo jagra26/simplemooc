5745123d579b06dd8f8f81bbe445fa0dace49775
soficastro <38478765+soficastro@users.noreply.github.com>
2019-08-16 08:58:39 -0300

    Add files via upload

---
+++ simplemooc/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+from .forms import RegisterForm
+
+@login_required
+def dashboard(request):
+	template_name = 'dashboard.html'
+	return render(request, template_name)
+
+def register(request):
+	template_name = 'register.html'
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			user = authenticate(
+				username=user.username, 
+				password=form.cleaned_data['password1']
+			)
+			login(request, user)
+			return redirect('/')
+
+	else:
+		form = RegisterForm()
+	context = {
+		'form': form
+	}
+	return render(request, template_name, context)
