0fac8a6db5907f09ef7dac2f63c5bfed94d367b3
soficastro <38478765+soficastro@users.noreply.github.com>
2019-08-17 08:26:52 -0300

    Update views.py

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-from .models import Course
+from .models import Course, Enrollment
 from .forms import ContactCourse
 
 def index(request):
@@ -11,14 +13,6 @@ def index(request):
 		'courses': courses
 	}
 	return render(request, template_name, context)
-	
-#def details(request, pk):
-#	course = get_object_or_404(Course, pk=pk)
-#	context = {
-#		'course': course
-#	}
-#	template_name = 'details.html'
-#	return render(request, template_name, context)
 
 def details(request, slug):
 	course = get_object_or_404(Course, slug=slug)
@@ -31,14 +25,58 @@ def details(request, slug):
 			form = ContactCourse()
 					
 	else:
-		form = ContactCourse()
-		
+		form = ContactCourse()	
 	context['form'] = form
 	context['course'] = course
-	
 	#context = {
 	#	'course': course,
 	#	'form': ContactCourse()
 	#}
 	template_name = 'details.html'
 	return render(request, template_name, context)
+
+@login_required
+def enrollment(request, slug):
+	course = get_object_or_404(Course, slug = slug)
+	enrollment, created = Enrollment.objects.get_or_create(user=request.user,
+		 course=course
+	)
+	if created:
+		#enrollment.active()
+		messages.success(request, 'Você foi inscrito no curso com sucesso')
+	else:
+		messages.info(request, 'Você já está inscrito no curso')
+	return redirect('dashboard')
+
+@login_required
+def undo_enrollment(request, slug):
+	course = get_object_or_404(Course, slug = slug)
+	enrollment = get_object_or_404(
+			Enrollment, user=request.user, course=course
+	)
+	if request.method == 'POST':
+		enrollment.delete()
+		messages.success(request, 'Inscrição cancelada com sucesso')
+		return redirect('dashboard')
+	template = 'undo_enrollment.html'
+	context = {
+		'enrollment': enrollment,
+		'course': course
+	}
+	return render(request, template, context)
+
+@login_required
+def announcements(request, slug):
+	course = get_object_or_404(Course, slug=slug)
+	if not request.user.is_staff:
+		enrollment = get_object_or_404(
+			Enrollment, user=request.user, course=course
+		)
+		if not enrollment.is_approved():
+			messages.error(request, 'A sua inscrição não ocorreu')
+	template = 'announcements.html'
+	context = {
+		'course': course,
+		'announcements': course.announcements.all()
+	}
+	return render(request, template, context)
