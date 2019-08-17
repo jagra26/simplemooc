718114a4f77bd8ca1a88dccc054f5dad3e15fe75
soficastro <38478765+soficastro@users.noreply.github.com>
2019-08-17 08:26:16 -0300

    Update urls.py

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from simplemooc.courses import views
-from simplemooc.courses.views import index, details
+from simplemooc.courses.views import index, details, enrollment, announcements, undo_enrollment
 urlpatterns = [ 
 	url(r'^$', index, name='index'),
 	url(r'^(?P<slug>[\w_-]+)/$', details, name='details'),
-	#url(r'^(?P<pk>\d+)/$', details, name='details'),
+	url(r'^(?P<slug>[\w_-]+)/inscricao/$', enrollment, name='enrollment'),
+	url(r'^(?P<slug>[\w_-]+)/anuncios/$', announcements, name='announcements'),
+	url(r'^(?P<slug>[\w_-]+)/cancelar-inscricao/$', undo_enrollment, name='undo_enrollment'),
 ]
